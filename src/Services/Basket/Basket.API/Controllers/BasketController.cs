ce9c85a2c4b07aaab3ff7b201a95143159011739
Justin Mulcahy <justin.mulcahy@gmail.com>
2022-05-10 17:24:48 +0100

    Basket

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,48 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+        private readonly Microsoft.Extensions.Logging.ILogger<BasketRepository> _logger;
+
+        public BasketController(IBasketRepository repository, ILogger<BasketRepository> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet("{userName}", Name ="GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]  
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string name)
+        {
+            var basket  = await _repository.GetBasket(name);
+            return Ok(basket  ?? new ShoppingCart(name));
+
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> DeleteBasket(string name)
+        {
+            await _repository.DeleteBasket(name);
+            return Ok();
+        }
+
+        [HttpPost("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _repository.UpdateBasket( basket));
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,17 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string name);
+
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+
+        Task DeleteBasket(string name);
+
+        
+
+    }
+}
